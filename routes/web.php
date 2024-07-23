f38b0d57bc46321ca7b33ee591b29a3721d3df34
Gobinda <gobindakk52@gmail.com>
2024-07-23 11:42:52 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'App\Http\Controllers\MoviesController@index')->name('movies.index');
+Route::get('/movies/{id}', 'App\Http\Controllers\MoviesController@show')->name('movies.show');
+
+Route::get('/tv', 'App\Http\Controllers\TvController@index')->name('tv.index');
+Route::get('/tv/{id}', 'App\Http\Controllers\TvController@show')->name('tv.show');
+
+Route::get('/actors', 'App\Http\Controllers\ActorsController@index')->name('actors.index');
+Route::get('/actors/page/{page?}', 'App\Http\Controllers\ActorsController@index');
+
+Route::get('/actors/{id}', 'App\Http\Controllers\ActorsController@show')->name('actors.show');
+
+
+
+
+
+
